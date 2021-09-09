2e7d0b5ec35b0714a6d628b286d8348257e92c4c
Sixtus Anyanwu <sparkdkiller@gmail.com>
2021-09-09 21:12:52 +0100

    added a function to views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def index(request):
+    data = {
+        "name": "John",
+        "age": 17,
+    }
+    return JsonResponse(data)
