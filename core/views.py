c39b9e5666e5716578135ee900f032c5d53695ed
Sixtus Anyanwu <sparkdkiller@gmail.com>
2021-09-14 10:11:29 +0100

    added authentication to views

--- core/views.py
+++ core/views.py
@@ -4,11 +4,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
+from rest_framework.permissions import IsAuthenticated
 
 
 class IndexAPI(APIView):
     """A class to override the get and post request..."""
 
+    permission_classes = (IsAuthenticated, ) # Allow only authenticate users to gain access to the api
+    
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         serializer = PostSerializer(qs, many=True)
--- drf_project/settings.py
+++ drf_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     #Third party apps
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES':
+        ['rest_framework.authentication.TokenAuthentication', ]
+}
