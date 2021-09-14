e39053b98f885ee2296e533584b500d76507336f
Sixtus Anyanwu <sparkdkiller@gmail.com>
2021-09-14 23:05:24 +0100

    changed the views to use a mixin

--- core/views.py
+++ core/views.py
@@ -5,22 +5,32 @@ from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, mixins
 
+from core import serializers
 
-class IndexAPI(APIView):
-    """A class to override the get and post request..."""
+class PostAPIView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    permission_classes = (IsAuthenticated, ) # Allow only authenticate users to gain access to the api
-    
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
         
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data= request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+        return self.list(self, request, *args, **kwargs)
+
+# class IndexAPI(APIView):
+#     """A class to override the get and post request..."""
+
+#     permission_classes = (IsAuthenticated, ) # Allow only authenticate users to gain access to the api
     
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+        
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data= request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+
--- drf_project/urls.py
+++ drf_project/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import IndexAPI
+from core.views import PostAPIView
 from rest_framework.authtoken.views import obtain_auth_token #A view used to return auth token for auth users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', IndexAPI.as_view(), name = "index"),
+    path('', PostAPIView.as_view(), name = "index"),
     path('api-token/', obtain_auth_token, name ="obtain-token"),
     path('rest-auth/', include('rest_auth.urls')),
 ]
