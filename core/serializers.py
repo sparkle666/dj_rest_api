2be7279e308e22170e268940c719a50d598a0f46
Sixtus Anyanwu <sparkdkiller@gmail.com>
2021-09-14 23:15:13 +0100

    added a createmodelmixin

--- core/serializers.py
+++ core/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'description')
+        fields = ( 'title', 'description', "user")
--- core/views.py
+++ core/views.py
@@ -9,13 +9,17 @@ from rest_framework import generics, mixins
 
 from core import serializers
 
-class PostAPIView(mixins.ListModelMixin, generics.GenericAPIView):
+class PostAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
     def get(self, request, *args, **kwargs):
         
-        return self.list(self, request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        return self.create(request, *args, **kwargs)
 
 # class IndexAPI(APIView):
 #     """A class to override the get and post request..."""
